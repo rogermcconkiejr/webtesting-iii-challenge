d8cdd33f831e7bb73f40d2d31627c4ec70c63f72
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-16 14:41:37 -0600

    gate unlocked and open text passed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,5 +3,13 @@ import React from 'react';
 import { render, fireEvent } from "@testing-library/react";
 
 test('should match snapshot', () => {
-    expect (render(<Dashboard/>)).toMatchSnapshot();
+    expect (render(<Dashboard />)).toMatchSnapshot();
 });
+
+//gate tests
+
+it('defaults to unlocked and open', ()=>{
+    const { getByText } = render( <Dashboard /> );
+    getByText(/open/i);
+    getByText(/unlocked/i);
+})
