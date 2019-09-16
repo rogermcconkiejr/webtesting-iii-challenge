e87fb862d00991810597673284b5ee258be0f68a
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-16 14:37:01 -0600

    snapshot test working

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,2 +1,7 @@
 import Dashboard from './Dashboard';
+import React from 'react';
 import { render, fireEvent } from "@testing-library/react";
+
+test('should match snapshot', () => {
+    expect (render(<Dashboard/>)).toMatchSnapshot();
+});
